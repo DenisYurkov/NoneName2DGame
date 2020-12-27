cf71750b388c26cf4919cd26d35d3334374e8296
geliou_23 <demonsoul9994@gmail.com>
2020-12-27 23:10:13 +0200

    Auto stash before rebase of "origin/DenisYurkov"

---
+++ Assets/Scripts/SwitchWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWeapon : MonoBehaviour
+{
+    public GameObject[] weapons;
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+
+        }
+    }
+
+}
