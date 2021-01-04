4f3ac0a6da492c15148bc79e5254a1e24d7df361
Denis Yurkov <denisdeveloper136@gmail.com>
2021-01-04 18:14:40 +0100

    Cinematic camera, end prototype Day 1

---
+++ Assets/Scripts/GhostSpeak.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpeak : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ghostText;
+    public GameObject ghostSay;
+    public GameObject ghost;
+    public Animator ghostAnimator;
+
+
+    public void Start()
+    {
+        ghostAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        HideGhostText();
+        if (collision.gameObject.tag == "Player")
+        {
+            ghostText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ghostSay.SetActive(true);
+                ghostAnimator.SetTrigger("GhostHide");
+                HideGhostSay();
+            }
+
+        }
+    }
+
+    private void GhostDestroy()
+    {
+        Destroy(ghost, 5f);
+    }
+
+    private void HideGhostText()
+    {
+        Destroy(ghost, 2f);
+    }
+
+    private void HideGhostSay()
+    {
+        Destroy(ghost, 4f);
+    }
+
+}
