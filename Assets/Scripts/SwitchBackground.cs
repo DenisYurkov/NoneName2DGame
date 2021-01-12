abc36a831a01a8f4bd9753522ed076a18a660151
geliou_23 <demonsoul9994@gmail.com>
2021-01-12 12:11:53 +0100

    New Assets

---
+++ Assets/Scripts/SwitchBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBackground : MonoBehaviour
+{
+    public Sprite layer_day, layer_night;
+    void Start()
+    {
+        Day();
+    }
+
+    void Day()
+    {
+        GetComponent<SpriteRenderer>().sprite = layer_day;
+        Invoke("Night", 40f);
+    }
+
+    void Night()
+    {
+        GetComponent<SpriteRenderer>().sprite = layer_night;
+        Invoke("Day", 40f);
+    }
+}
+
